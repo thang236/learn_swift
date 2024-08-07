ba5498123040df36081a1382d2157e0a3bb3ebb8
Louis hoàng <thang.uha236@gmail.com>
2024-08-07 14:41:37 +0700

    e gui lan 2 :airplane:

--- learn_swift/CollectionViewCell.swift
+++ learn_swift/CollectionViewCell.swift
@@ -10,8 +10,15 @@ import UIKit
 class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
+
     
-    @IBOutlet weak var titleLBL: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
-    
+    func setupData(Car car: Car) {
+        titleLabel.text = car.titile
+        imageView.image = UIImage(named: car.image)
+        
+        layer.borderWidth = 1
+        layer.cornerRadius = 23
+    }
 }
